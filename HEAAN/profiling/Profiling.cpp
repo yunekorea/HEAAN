72330ba74d5f350f1253815d8f665bfe51262aa7
yunekorea <kooopooo@naver.com>
2025-01-26 02:29:29 -0500

    Moved functions for loading/saving from/to a file to Profiling.cpp.

--- HEAAN/profiling/Profiling.cpp
+++ HEAAN/profiling/Profiling.cpp
@@ -4,28 +4,11 @@ using namespace std;
 using namespace NTL;
 using namespace heaan;
 
-int loadDouble(int FileName) {
-    /*
-    std::ifstream inFile(FileName);
-    if (!inFile) {
-        throw std::runtime_error("Failed to open file for loading doubles.");
-    }
-    //complex<double>* loaded = new complex<double>[n];
-    std::vector<std::complex<double>> loadedVector;
-    complex<double> number;
-    while (inFile >> number) {
-        // Create a complex number with the integer as the real part and 0 as the imaginary part
-        //std::complex<double> complexNumber(number, 0);
-        complex<double> complexNumber = number;
+int loadCiphertext(string FileName, Ciphertext &ciphertext);
+std::complex<double>* loadInt(std::string FileName);
+std::complex<double>* loadDouble(const std::string FileName);
 
-        // Add the complex number to the array
-        loadedVector.push_back(complexNumber);
-    }
-
-    return &loadedVector[0];
-    */
-    return 0;
-}
+int saveCiphertext(Ciphertext &ciphertext, std::string FileName);
 
 int main(int argc, char **argv) {
   /*
@@ -100,4 +83,78 @@ int main(int argc, char **argv) {
 
   return 0;
 
+}
+
+int loadCiphertext(string FileName, Ciphertext &ciphertext) {
+    /* 
+    std::ifstream inFile(filename, std::ios::binary);
+    if (!inFile) {
+        throw std::runtime_error("Failed to open file for loading ciphertext.");
+    }
+
+    // Read ciphertext parameters
+    inFile.read(reinterpret_cast<char*>(&ciphertext.logp), sizeof(long));
+    inFile.read(reinterpret_cast<char*>(&ciphertext.logq), sizeof(long)); 
+    inFile.read(reinterpret_cast<char*>(&ciphertext.n), sizeof(long));
+
+    // Resize the underlying vectors
+    ciphertext.ax.resize(ciphertext.n);
+    ciphertext.bx.resize(ciphertext.n);
+
+    // Read ciphertext values
+    for (auto& coeff : ciphertext.ax) {
+        inFile.read(reinterpret_cast<char*>(&coeff), sizeof(uint64_t));
+    }
+    for (auto& coeff : ciphertext.bx) {
+        inFile.read(reinterpret_cast<char*>(&coeff), sizeof(uint64_t));
+    }
+
+    inFile.close();
+    */
+    return 0;
+}
+
+std::complex<double>* loadDouble(const std::string FileName {
+    /*
+    std::ifstream inFile(FileName);
+    if (!inFile) {
+        throw std::runtime_error("Failed to open file for loading doubles.");
+    }
+    //complex<double>* loaded = new complex<double>[n];
+    std::vector<std::complex<double>> loadedVector;
+    complex<double> number;
+    while (inFile >> number) {
+        // Create a complex number with the integer as the real part and 0 as the imaginary part
+        //std::complex<double> complexNumber(number, 0);
+        complex<double> complexNumber = number;
+
+        // Add the complex number to the array
+        loadedVector.push_back(complexNumber);
+    }
+
+    return &loadedVector[0];
+    */
+    return 0;
+}
+
+int saveCiphertext(Ciphertext &ciphertext, std::string FileName) {
+    /*
+    std::ofstream outFile("integer1.cip", std::ios::binary);
+    if (!outFile) {
+        throw std::runtime_error("Failed to open file for saving ciphertext.");
+    }
+    outFile.write(reinterpret_cast<const char*>(&ciphertext.logp), sizeof(long));
+    outFile.write(reinterpret_cast<const char*>(&ciphertext.logq), sizeof(long));
+    outFile.write(reinterpret_cast<const char*>(&ciphertext.n), sizeof(long));
+
+     for (const auto& coeff : ciphertext.ax) {
+        outFile.write(reinterpret_cast<const char*>(&coeff), sizeof(uint64_t));
+    }
+    for (const auto& coeff : ciphertext.bx) {
+        outFile.write(reinterpret_cast<const char*>(&coeff), sizeof(uint64_t));
+    }
+
+    outFile.close();
+    */
+    return 0;
 }

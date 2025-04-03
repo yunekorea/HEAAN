b23aac0bf3ae381aa9f370da3b164851b84f3d32
yunekorea <kooopooo@naver.com>
2025-04-03 17:58:24 +0900

    Added a shell script for profiling experiments.

---
+++ HEAAN/profiling/run_prof.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+oper=$1
+savedir=prof_data_$1
+
+mkdir ${savedir}
+
+# Start profiling tools
+sar -u 1 -o ${savedir}/sar_cpu_${oper}.file > /dev/null 2>&1 &
+sar -d --dev=nvme0n1 1 -o ${savedir}/sar_disk_${oper}.file > /dev/null 2>&1 &
+sudo blktrace -d /dev/nvme0n1 -o ${savedir}/blktrace_${oper} &
+
+# Run profheaan
+./ProfilingHEAAN ${oper}
+
+# Stop profiling tools
+sudo killall blktrace
+killall sar
+
+# Process data
+#blkparse -i blktrace_${oper} -o blktrace_${oper}.parsed
+iowatcher -t ${savedir}/blktrace_${oper} -o ${savedir}/iowatcher_${oper}.svg
+
+echo "Profiling complete. Check output.png and sar logs."
+

51ceddb7b95e161d942d78922e6873dacfd9e9d1
yunekorea <kooopooo@naver.com>
2025-04-15 19:47:44 +0900

    Progress printing function is changed to a minimal way.

--- HEAAN/profiling/run_prof.sh
+++ HEAAN/profiling/run_prof.sh
@@ -1,19 +1,22 @@
 #!/bin/zsh
 
 oper=$1
-savedir=prof_data_$1
+dir=prof_data_$1
 
-mkdir ${savedir}
+#make directory for profiled data
+mkdir ${dir}
+savedir=$PWD/${dir}
 
+echo ${savedir}
 # Start profiling tools
 sar -u 1 -o ${savedir}/sar_cpu_${oper}.file > /dev/null 2>&1 &
 sar -r 1 -o ${savedir}/sar_memory_${oper}.file > /dev/null 2>&1 &
 sar -b 1 -o ${savedir}/sar_io_${oper}.file > /dev/null 2>&1 &
 sar -d 1 --dev=nvme0n1 -o ${savedir}/sar_disk_${oper}.file > /dev/null 2>&1 &
 sar -B 1 -o ${savedir}/sar_paging_${oper}.file > /dev/null 2>&1 &
-sudo blktrace -d /dev/nvme0n1 -o ${savedir}/blktrace_${oper} &
+sudo blktrace -d /dev/nvme0n1 -o ${dir}/blktrace_${oper} &
 
-# Run profheaan
+# Run profiling
 ./ProfilingHEAAN ${oper}
 
 # Stop profiling tools
@@ -23,11 +26,15 @@ killall sar
 sleep 5
 
 # Process data
-#blkparse -i blktrace_${oper} -o blktrace_${oper}.parsed
-iowatcher -t $PWD/${savedir}/blktrace_${oper} -o $PWD/${savedir}/iowatcher_${oper}.svg
-
+iowatcher -t ${savedir}/blktrace_${oper} -o ${savedir}/iowatcher_${oper}.svg
 sleep 5
+blkparse -i ${savedir}/blktrace_${oper} -d ${savedir}/blkparse_${oper}_dump.bin
 
+cd ${savedir}
+btt -i ${savedir}/blkparse_${oper}_dump.bin -o ${savedir}/btt_${oper}
+cd ../
+
+# make output text file from sar binary output
 sar -u -f ${savedir}/sar_cpu_${oper}.file > ${savedir}/sar_cpu_${oper}.out
 sar -r -f ${savedir}/sar_memory_${oper}.file > ${savedir}/sar_memory_${oper}.out
 sar -b -f ${savedir}/sar_io_${oper}.file > ${savedir}/sar_io_${oper}.out

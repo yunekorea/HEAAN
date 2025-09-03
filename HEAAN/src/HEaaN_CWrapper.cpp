73fd7b3a365c474b867961e27c3cd9c3a4cc7021
yunekorea <kooopooo@naver.com>
2025-09-03 17:08:14 +0900

    Added wrapper functions to create cpp classes.

--- HEAAN/src/HEaaN_CWrapper.cpp
+++ HEAAN/src/HEaaN_CWrapper.cpp
@@ -1,7 +1,33 @@
 #include "HEaaN_CWrapper.h"
 
+using namespace std;
+using namespace NTL;
+using namespace heaan;
+
 extern "C" {
 
+void* create_Ciphertext(void) {
+  return new Ciphertext();
+}
 
+void* create_Ring(void) {
+  return new Ring();
+}
+
+void* create_SecretKey(void* ring) {
+  return new SecretKey((Ring*)ring);
+}
+
+void* create_Scheme(void* secretKey, void* ring) {
+  return new Scheme((secretKey*) secretKey, (Ring*) ring);
+}
+
+void addLeftRotKeys(void* scheme, void *secretKey) {
+  ((Scheme*)scheme).addLeftRotKeys((SecretKey*)secretKey);
+}
+
+void addRightRotKeys(void* scheme, void *secretKey) {
+  ((Scheme*)scheme).addRightRotKeys((SecretKey*)secretKey);
+}
 
 }

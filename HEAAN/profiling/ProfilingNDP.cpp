26d2127990984d8e681912c454bdb3163e1d07b1
yunekorea <kooopooo@naver.com>
2025-12-04 17:44:56 +0900

    Modified cipadd to commit NDP offloading.

--- HEAAN/profiling/ProfilingNDP.cpp
+++ HEAAN/profiling/ProfilingNDP.cpp
@@ -6,6 +6,7 @@ using namespace heaan;
 
 
 std::complex<double>* loadDouble(const std::string FileName);
+void* data_buffer_alloc(size_t len);
 
 int main(int argc, char **argv) {
   /*
@@ -17,6 +18,11 @@ int main(int argc, char **argv) {
   std::string SFileName1 = "randint_cipher_1024_1_";
   std::string SFileNameAdd = "randint_cipher_1024_Add_";
   std::string WorkingDir = "/mnt/nvmf";
+  int nvme_fd = nvme_open("/dev/nvme1n1");
+  if(nvme_fd == -1) {
+    cout << "Opening the file descriptor has failed." << endl;
+    return -1;
+  }
 
   // Parameters //
   long logq = 300; ///< Ciphertext modulus (this value should be <= logQ in "scr/Params.h")
@@ -24,7 +30,6 @@ int main(int argc, char **argv) {
   long logn = 10; ///< number of slot is 1024 (this value should be < logN in "src/Params.h")
   long n = 1 << logn;
   long slots = n;
-  long numThread = 8;
   
   //int iter = 214600;
   int iter = 1024;
@@ -33,6 +38,7 @@ int main(int argc, char **argv) {
   cout << "Selected Operation : " << oper << endl;
   // Construct and Generate Public Keys //
   srand(time(NULL));
+  long numThread = 8;
   SetNumThreads(numThread);
   TimeUtils timeutils;
   Ring ring;
@@ -76,31 +82,73 @@ int main(int argc, char **argv) {
 
   // Load/add ciphertexts //
   if(oper == "cipadd"){
-  Ciphertext* cipher2[iter];
-  Ciphertext* cipher3[iter];
-  Ciphertext cipherAdd[iter];
-  std::string temp;
-  char* input_0_path = WorkingDir.data();
-  char* input_0_filename = NULL;
-  char* input_1_path = WorkingDir.data();
-  char* input_1_filename = NULL;
-  char* target_path = WorkingDir.data();
-  char* target_filename = NULL;
-  //complex<double>* dvecadd[iter];
-  for(int i = 0; i < iter; i++) { 
-    cout << "Progress : " << i << "/" << iter-1 << "\r";
+    std::string temp;
+    char* input_0_path = NULL;
+    char* input_0_filename = NULL;
+    char* input_1_path = NULL;
+    char* input_1_filename = NULL;
+    char* target_path = NULL;
+    char* target_filename = NULL;
+    //complex<double>* dvecadd[iter];
+    for(int i = 0; i < iter; i++) { 
+      cout << "Progress : " << i << "/" << iter-1 << "\r";
+      
+      temp = WorkingDir + "/1GB_ciphertexts/";
+      strcpy(input_0_path, temp.c_str());
+      temp = SFileName0 + std::to_string(i) + ".cip";
+      strcpy(input_0_filename, temp.c_str());
+      
+      temp = WorkingDir + "/1GB_ciphertexts/";
+      strcpy(input_1_path, temp.c_str());
+      temp = SFileName1 + std::to_string(i) + ".cip";
+      strcpy(input_1_filename, temp.c_str());
+      
+      temp = WorkingDir + "/1GB_ciphertexts/";
+      strcpy(target_path, temp.c_str());
+      temp = SFileNameAdd + std::to_string(i) + ".cip";
+      strcpy(target_filename, temp.c_str());
+
+      size_t full_string_len = strlen(input_0_path) + strlen(input_0_filename) +
+									strlen(input_1_path) + strlen(input_1_filename) +
+									strlen(target_path) + strlen(target_filename) + 5;
+      // create metadata for ioctl
+      ndp_passthru_cmd cipadd_cmd = {
+        .opcode = 0xe0,
+        .flags = 0,
+        .rsvd = 0,
+        .nsid = 1,
+        .cdw2 = 0,
+        .cdw3 = 0,
+        .cdw10 = 0,
+        .cdw11 = 0,
+        .cdw12 = 0,
+        .cdw13 = 0,
+        .cdw14 = 0,
+        .cdw15 = 0,
+        .data_len = 4096,
+        .data = NULL,
+        .metadata_len = 0,
+        .metadata = NULL,
+        .timeout_ms = 60000,
+        .result = 0,
+      };
+
+      void* data_buffer = data_buffer_alloc(cipadd_cmd.data_len);
+      std::snprintf((char*)data_buffer, full_string_len,
+              "%s|%s|%s|%s|%s|%s",
+              input_0_path, input_0_filename,
+              input_1_path, input_1_filename,
+              target_path, target_filename);
+      
+      cipadd_cmd.data = data_buffer;
+
+      int result = nvme_admin_passthru64(nvme_fd, cipadd_cmd.opcode, cipadd_cmd.flags,
+                      cipadd_cmd.rsvd, cipadd_cmd.nsid, cipadd_cmd.cdw2, cipadd_cmd.cdw3,
+                      cipadd_cmd.cdw10, cipadd_cmd.cdw11, cipadd_cmd.cdw12, cipadd_cmd.cdw13,
+                      cipadd_cmd.cdw14, cipadd_cmd.cdw15, cipadd_cmd.data_len, cipadd_cmd.data,
+                      cipadd_cmd.metadata_len, cipadd_cmd.metadata, cipadd_cmd.timeout_ms, 
+                      &cipadd_cmd.result);
     
-    temp = "/1GB_ciphertexts/" + SFileName0 + std::to_string(i) + ".cip";
-    input_0_filename = temp.data();
-    temp = "/1GB_ciphertexts/" + SFileName1 + std::to_string(i) + ".cip";
-    input_1_filename = temp.data();
-    temp = "/1GB_ciphertexts/" + SFileNameAdd + std::to_string(i) + ".cip";
-    target_filename = temp.data();
-
-    // create metadata for ioctl
-
-
-    //ioctl
     }
     cout << endl;
   }
@@ -230,3 +278,12 @@ std::complex<double>* loadDouble(const std::string FileName) {
   return 0;
 }
 
+void* data_buffer_alloc(size_t len) {
+  void *p;
+  len = ROUND_UP(len, 0x1000);
+  if (posix_memalign(&p, getpagesize(), len))
+		return NULL;
+
+	memset(p, 0, len);
+	return p;
+}
--- HEAAN/profiling/ProfilingNDP.h
+++ HEAAN/profiling/ProfilingNDP.h
@@ -8,6 +8,13 @@
 #include <linux/fs.h>
 #include <linux/fiemap.h>
 
+extern "C" {
+    #include <libnvme.h>
+    #include <unistd.h>
+}
+
+#define ROUND_UP(N, S) ((((N) + (S) - 1) / (S)) * (S))
+
 typedef struct {
     uint64_t logical_offset;
     uint64_t physical_offset;
@@ -22,5 +29,26 @@ typedef struct {
     uint64_t total_length;
     extent_info_t *extents;
 } file_layout_t;
+
+typedef struct {
+    __u8	opcode;
+	__u8	flags;
+	__u16	rsvd;
+	__u32	nsid;
+	__u32	cdw2;
+	__u32	cdw3;
+	__u32	cdw10;
+	__u32	cdw11;
+	__u32	cdw12;
+	__u32	cdw13;
+	__u32	cdw14;
+	__u32	cdw15;
+	__u32	data_len;
+	void*	data;
+	__u32	metadata_len;
+	void*   metadata;
+	__u32	timeout_ms;
+	__u64	result;
+} ndp_passthru_cmd;
 
 #endif
